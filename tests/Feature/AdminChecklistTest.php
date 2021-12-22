2708ad20726cd8b70f736330d7935c92d4ee8eda
giezele <giedre.zel@gmail.com>
2021-12-22 12:24:28 +0200

    Unit tests

--- tests/Feature/AdminChecklistTest.php
+++ tests/Feature/AdminChecklistTest.php
@@ -21,11 +21,13 @@ class AdminChecklistTest extends TestCase
      *
      * @return void
      */
-    public function test_example(): void
+    public function test_manage_checklist_groups(): void
     {
         // parent::setUp();
 
         $admin = User::factory()->create(['is_admin' => 1]);
+
+        //test create
         $response = $this->actingAs($admin)->post('admin/checklist_groups', [
             'name' => 'First group'
         ]);
@@ -34,6 +36,7 @@ class AdminChecklistTest extends TestCase
         $group = ChecklistGroup::where('name', 'First group')->first();
         $this->assertNotNull($group);
 
+        //test edit
         $response = $this->actingAs($admin)->get('admin/checklist_groups/' . $group->id . '/edit');
         $response->assertStatus(200);
 
@@ -48,5 +51,132 @@ class AdminChecklistTest extends TestCase
 
         $menu = (new MenuService())->get_menu();
         $this->assertEquals(1, $menu['admin_menu']->where('name', '=', 'Updated first group')->count());
+        
+        //test delete
+        $response = $this->actingAs($admin)->delete('admin/checklist_groups/' . $group->id);
+        $response->assertRedirect('welcome');
+
+        $group = ChecklistGroup::where('name', '=', 'Updated first group')->first();
+        $this->assertNull($group);
+
+        $menu = (new MenuService())->get_menu();
+        $this->assertEquals(0, $menu['admin_menu']->where('name', '=', 'Updated first group')->count()); 
+    }
+
+    public function test_manage_checklists(): void
+    {
+        $admin = User::factory()->create(['is_admin' => 1]);
+        $checklist_group = ChecklistGroup::factory()->create();
+
+        $checklist_url = 'admin/checklist_groups/' . $checklist_group->id . '/checklists';
+
+        //test create
+        $response = $this->actingAs($admin)->get($checklist_url . '/create');
+        $response->assertStatus(200);
+
+        $response = $this->actingAs($admin)->post($checklist_url, [
+            'name' => 'First checklist'
+        ]);
+        $response->assertRedirect('welcome');
+
+        $checklist = Checklist::where('name', '=', 'First checklist')->first();
+        $this->assertNotNull($checklist);
+
+        //test edit
+        $response = $this->actingAs($admin)->get($checklist_url . '/create');
+        $response->assertStatus(200);
+
+        $response = $this->actingAs($admin)->post($checklist_url, [
+            'name' => 'Updated checklist'
+        ]);
+        $response->assertRedirect('welcome');
+
+        $checklist = Checklist::where('name', '=', 'Updated checklist')->first();
+        $this->assertNotNull($checklist);
+
+        $menu = (new MenuService())->get_menu();
+        $this->assertTrue($menu['admin_menu']->first()->checklists->contains($checklist));
+
+        //test delete
+        $response = $this->actingAs($admin)->delete($checklist_url . '/' . $checklist->id);
+        $response->assertRedirect('welcome');
+
+        $deleted_checklist = Checklist::where('name', '=', 'Updated checklist')->first();
+        $this->assertNull($deleted_checklist);
+
+        $menu = (new MenuService())->get_menu();
+        $this->assertFalse($menu['admin_menu']->first()->checklists->contains($checklist));
+    }
+
+    public function test_manage_tasks()
+    {
+        $admin = User::factory()->create(['is_admin' => 1]);
+        $checklist_group = ChecklistGroup::factory()->create();
+        $checklist = Checklist::factory()->create(['checklist_group_id' => $checklist_group->id]);
+
+        $tasks_url = 'admin/checklists/' . $checklist->id . '/tasks';
+        $response = $this->actingAs($admin)->post($tasks_url, [
+            'name' => 'Some task',
+            'description' => 'Some description'
+        ]);
+        $response->assertRedirect('admin/checklist_groups/' . $checklist_group->id . '/checklists/' . $checklist->id . '/edit');
+
+        $task = Task::where('name', 'Some task')->first();
+        $this->assertNotNull($task);
+        $this->assertEquals(1, $task->position);
+
+        $response = $this->actingAs($admin)->put($tasks_url . '/' . $task->id, [
+            'name' => 'Updated task',
+            'description' => $task->description
+        ]);
+        $response->assertRedirect('admin/checklist_groups/' . $checklist_group->id . '/checklists/' . $checklist->id . '/edit');
+
+        $task = Task::where('name', 'Updated task')->first();
+        $this->assertNotNull($task);
+    }
+
+    public function test_delete_task_with_position_reordered()
+    {
+        $admin = User::factory()->create(['is_admin' => 1]);
+        $checklist_group = ChecklistGroup::factory()->create();
+        $checklist = Checklist::factory()->create(['checklist_group_id' => $checklist_group->id]);
+
+        $task1 = Task::factory()->create(['checklist_id' => $checklist->id, 'position' => 1]);
+        $task2 = Task::factory()->create(['checklist_id' => $checklist->id, 'position' => 2]);
+
+        $tasks_url = 'admin/checklists/' . $checklist->id . '/tasks';
+        $response = $this->actingAs($admin)->delete($tasks_url . '/' . $task1->id);
+        $response->assertRedirect('admin/checklist_groups/' . $checklist_group->id . '/checklists/' . $checklist->id . '/edit');
+
+        $task = Task::where('name', $task1->name)->first();
+        $this->assertNull($task);
+
+        $task = Task::where('name', $task2->name)->first();
+        $this->assertNotNull($task);
+        $this->assertEquals(1, $task->position);
+    }
+
+    public function test_reordering_task_with_livewire()
+    {
+        $admin = User::factory()->create(['is_admin' => 1]);
+        $checklist_group = ChecklistGroup::factory()->create();
+        $checklist = Checklist::factory()->create(['checklist_group_id' => $checklist_group->id]);
+
+        $task1 = Task::factory()->create(['checklist_id' => $checklist->id, 'position' => 1]);
+        $task2 = Task::factory()->create(['checklist_id' => $checklist->id, 'position' => 2]);
+
+        $this->actingAs($admin);
+
+        Livewire::test(TasksTable::class, ['checklist' => $checklist])
+            ->call('task_up', $task2->id);
+
+        $task = Task::find($task2->id);
+        $this->assertEquals(1, $task->position);
+
+        Livewire::test(TasksTable::class, ['checklist' => $checklist])
+            ->call('task_down', $task2->id);
+
+        $task = Task::find($task2->id);
+        $this->assertEquals(2, $task->position);
     }
 }
